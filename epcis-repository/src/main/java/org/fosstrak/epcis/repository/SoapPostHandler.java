0e24277ee3ea7de6793d177ef050fe5b58995bdf
steybem <marco.steybe@gmail.com>
2007-01-20 10:28:34 +0000

    initialization handling: opening db, loading subscr, ...

---
+++ epcis-repository/src/main/java/org/fosstrak/epcis/repository/SoapPostHandler.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.accada.epcis.repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+
+import org.accada.epcis.soapapi.ImplementationException;
+import org.accada.epcis.soapapi.ImplementationExceptionSeverity;
+import org.apache.axis.MessageContext;
+import org.apache.axis.handlers.BasicHandler;
+import org.apache.axis.transport.http.HTTPConstants;
+
+/**
+ * @author Marco Steybe
+ */
+public class SoapPostHandler extends BasicHandler {
+
+    /**
+     * Invokes this SoapPostHandler which performs post processing for the EPCIS
+     * query service, such as closing the database.
+     * 
+     * @see org.apache.axis.Handler#invoke(org.apache.axis.MessageContext)
+     * @param msgContext
+     *            the MessageContext to process with this InitHandler.
+     * @throws ImplementationException
+     *             If an error reading the configuration or setting up the
+     *             database occured.
+     */
+    public void invoke(MessageContext msgContext)
+            throws ImplementationException {
+        try {
+            Connection dbconnection = (Connection) msgContext.getProperty("dbconnection");
+            dbconnection.close();
+
+            Map<String, SubscriptionScheduled> subscribedMap = (HashMap<String, SubscriptionScheduled>) msgContext.getProperty("subscribedMap");
+            HttpServlet servlet = (HttpServlet) msgContext.getProperty(HTTPConstants.MC_HTTP_SERVLET);
+            servlet.getServletContext().setAttribute("subscribedMap",
+                    subscribedMap);
+        } catch (SQLException e) {
+            ImplementationException iex = new ImplementationException();
+            String msg = "Unable to close the database connection: " + e.getMessage();
+            iex.setReason(msg);
+            iex.setStackTrace(e.getStackTrace());
+            iex.setSeverity(ImplementationExceptionSeverity.ERROR);
+            throw iex;
+        }
+    }
+
+}
---
+++ epcis-repository/src/main/java/org/fosstrak/epcis/repository/SoapPreHandler.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package org.accada.epcis.repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
+
+import org.accada.epcis.soapapi.ImplementationException;
+import org.accada.epcis.soapapi.ImplementationExceptionSeverity;
+import org.apache.axis.MessageContext;
+import org.apache.axis.handlers.BasicHandler;
+import org.apache.axis.transport.http.HTTPConstants;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * @author Marco Steybe
+ */
+public class SoapPreHandler extends BasicHandler {
+
+    /**
+     * Invokes this SoapPreHandler which performs the initialization for the EPCIS
+     * query service, such as reading configuration and setting up the database.
+     * 
+     * @see org.apache.axis.Handler#invoke(org.apache.axis.MessageContext)
+     * @param msgContext
+     *            the MessageContext to process with this InitHandler.
+     * @throws ImplementationException
+     *             If an error reading the configuration or setting up the
+     *             database occured.
+     */
+    public void invoke(MessageContext msgContext)
+            throws ImplementationException {
+        try {
+            Context initContext = new InitialContext();
+            Context env = (Context) initContext.lookup("java:comp/env");
+            DataSource db = (DataSource) env.lookup("jdbc/EPCISDB");
+
+            Connection dbconnection = db.getConnection();
+            String delimiter = dbconnection.getMetaData().getIdentifierQuoteString();
+
+            ServletContext ctx = ((HttpServlet) msgContext.getProperty(HTTPConstants.MC_HTTP_SERVLET)).getServletContext();
+            String servletPath = ctx.getRealPath("/");
+            String log4jConfigFile = ctx.getInitParameter("log4jConfigFile");
+            if (log4jConfigFile != null) {
+                // if no log4j properties file found, then do not try
+                // to load it (the application runs without logging)
+                PropertyConfigurator.configure(servletPath + log4jConfigFile);
+            }
+
+            Map<String, SubscriptionScheduled> subscribedMap = (HashMap<String, SubscriptionScheduled>) ctx.getAttribute("subscribedMap");
+
+            msgContext.setProperty("dbconnection", dbconnection);
+            msgContext.setProperty("delimiter", delimiter);
+            msgContext.setProperty("subscribedMap", subscribedMap);
+        } catch (NamingException e) {
+            ImplementationException iex = new ImplementationException();
+            String msg = "Unable to read configuration, check META-INF/context.xml for Resource 'jdbc/EPCISDB'.";
+            iex.setReason(msg);
+            iex.setStackTrace(e.getStackTrace());
+            iex.setSeverity(ImplementationExceptionSeverity.ERROR);
+            throw iex;
+        } catch (SQLException e) {
+            ImplementationException iex = new ImplementationException();
+            String msg = "Unable to connect to the database: " + e.getMessage();
+            iex.setReason(msg);
+            iex.setStackTrace(e.getStackTrace());
+            iex.setSeverity(ImplementationExceptionSeverity.ERROR);
+            throw iex;
+        }
+    }
+
+}
