6f78e75228fdf739b0dbdfece88123b1f4564386
roduner <christof.roduner@gmail.com>
2007-04-05 10:56:53 +0000

    slightly updated license notice

--- epcis-commons/src/main/java/org/fosstrak/epcis/soapapi/EPC.java
+++ epcis-commons/src/main/java/org/fosstrak/epcis/soapapi/EPC.java
@@ -1,8 +1,21 @@
-/**
- * EPC.java
+/*
+ * Copyright (C) 2007 ETH Zurich
  *
- * This file was auto-generated from WSDL
- * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
  */
 
 package org.accada.epcis.soapapi;
