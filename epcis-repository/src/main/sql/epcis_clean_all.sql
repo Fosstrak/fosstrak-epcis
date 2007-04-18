372345632ac29c6182bb7086a46dde0f84e625ac
roduner <christof.roduner@gmail.com>
2007-04-18 07:43:05 +0000

    added licensing information in header

--- epcis-repository/src/main/sql/epcis_clean_all.sql
+++ epcis-repository/src/main/sql/epcis_clean_all.sql
@@ -1,3 +1,22 @@
+-- Copyright (C) 2007 ETH Zurich
+--
+-- This file is part of Accada (www.accada.org).
+--
+-- Accada is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License version 2.1, as published by the Free Software Foundation.
+--
+-- Accada is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with Accada; if not, write to the Free
+-- Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+-- Boston, MA  02110-1301  USA
+--
+--
 -- deletes all data from all the tables in your epcis db
 
 BEGIN;
