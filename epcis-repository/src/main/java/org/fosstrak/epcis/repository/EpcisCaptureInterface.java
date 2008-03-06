27e8bf82b3d7e3bfc470026c012d3645883640bb
steybem <marco.steybe@gmail.com>
2008-03-06 15:37:55 +0000

    added EpcisCaptureInterface (but there is currently no implementation of it - see TODO note in CaptureOperationsModule)

---
+++ epcis-repository/src/main/java/org/fosstrak/epcis/repository/EpcisCaptureInterface.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.epcis.repository;
+
+import java.util.List;
+
+import org.accada.epcis.repository.model.EPCISEvent;
+
+/**
+ * The EPCIS Capture Interface defines the delivery of EPCIS events from EPCIS
+ * Capturing Applications to an EPCIS Repository. The interface contains only a
+ * single method, <code>capture</code>, which takes a single argument and
+ * returns no results. Implementations of the EPCIS Capture Interface SHALL
+ * accept each element of the argument list that is a valid
+ * <code>EPCISEvent</code> or subtype thereof according to the EPCIS
+ * specification. Implementations MAY accept other types of events through
+ * vendor extension.
+ */
+public interface EpcisCaptureInterface {
+
+    /**
+     * The <code>capture</code> operation records one or more EPCIS events, of
+     * any type.
+     * 
+     * @param events
+     *            The event(s) to capture. All relevant information such as the
+     *            event time, EPCs, etc., are contained within each event.
+     *            Exception: the <code>recordTime</code> MAY be omitted.
+     *            Whether the <code>recordTime</code> is omitted or not in the
+     *            input, following the capture operation the
+     *            <code>recordTime</code> of the event as recorded by the
+     *            EPCIS Repository or EPCIS Accessing Application is the time of
+     *            capture.
+     */
+    public void capture(List<EPCISEvent> events);
+}
