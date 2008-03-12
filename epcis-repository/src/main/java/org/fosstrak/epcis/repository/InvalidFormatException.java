7fd0ef4bea9f9e8e3adc2a318d6a89a33d4814c5
steybem <marco.steybe@gmail.com>
2008-03-12 22:46:57 +0000

    added missing exception class used in capture module

---
+++ epcis-repository/src/main/java/org/fosstrak/epcis/repository/InvalidFormatException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.epcis.repository;
+
+/**
+ * TODO: javadoc!
+ * 
+ * @author Marco Steybe
+ */
+public class InvalidFormatException extends Exception {
+
+    private static final long serialVersionUID = 1390149913827592956L;
+
+    public InvalidFormatException() {
+        super();
+    }
+
+    public InvalidFormatException(String message) {
+        super(message);
+    }
+
+    public InvalidFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
