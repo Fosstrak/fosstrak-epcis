229194c9f8862698b4648911fa3787b78bac70ef
steybem <marco.steybe@gmail.com>
2010-08-05 13:40:03 +0000

    added sample code how to query masterdata using the jaxb-generated model

--- epcis-queryclient/src/test/java/org/fosstrak/epcis/queryclient/SimpleMasterDataQueryTest.java
+++ epcis-queryclient/src/test/java/org/fosstrak/epcis/queryclient/SimpleMasterDataQueryTest.java
@@ -22,6 +22,10 @@ package org.fosstrak.epcis.queryclient;
 
 import java.net.URL;
 
+import org.fosstrak.epcis.model.ArrayOfString;
+import org.fosstrak.epcis.model.Poll;
+import org.fosstrak.epcis.model.QueryParam;
+import org.fosstrak.epcis.model.QueryParams;
 import org.fosstrak.epcis.model.QueryResults;
 import org.fosstrak.epcis.utils.QueryResultsParser;
 
@@ -46,11 +50,17 @@ public class SimpleMasterDataQueryTest {
         client.configureService(new URL(queryUrl), null);
 
         // create a query in its XML form and send it to the repository
-        System.out.println("Sending query:");
         String xmlQuery = createPollXml();
+        System.out.println("Sending query:");
         System.out.println(xmlQuery);
         QueryResults results = client.poll(xmlQuery);
         // print the results to System.out
+        QueryResultsParser.queryResultsToXml(results, System.out);
+        
+        // create a query Poll object and send it to the query service
+        Poll poll = createPoll();
+        System.out.println("Sending query:");
+        results = client.poll(poll);
         QueryResultsParser.queryResultsToXml(results, System.out);
     }
 
@@ -72,10 +82,42 @@ public class SimpleMasterDataQueryTest {
         sb.append("  </param>\n");
         sb.append("  <param>\n");
         sb.append("    <name>EQ_name</name>\n");
-        sb.append("    <value><string>urn:epcglobal:fmcg:loc:0614141073467</string></value>\n");
+        sb.append("    <value><string>urn:epc:id:sgln:0614141.00729.shipping</string></value>\n");
         sb.append("  </param>\n");
         sb.append("</params>\n");
         sb.append("</epcisq:Poll>");
         return sb.toString();
+    }
+
+    /**
+     * Creates and returns a simple EPCIS query Poll object.
+     */
+    private static Poll createPoll() {
+        // construct the query parameters
+        QueryParam queryParam1 = new QueryParam();
+        queryParam1.setName("includeAttributes");
+        queryParam1.setValue("true");
+
+        QueryParam queryParam2 = new QueryParam();
+        queryParam2.setName("includeChildren");
+        queryParam2.setValue("true");
+
+        QueryParam queryParam3 = new QueryParam();
+        queryParam3.setName("EQ_name");
+        ArrayOfString queryParamValue3 = new ArrayOfString();
+        queryParamValue3.getString().add("urn:epc:id:sgln:0614141.00729.shipping");
+        queryParam3.setValue(queryParamValue3);
+
+        // add the query parameters to the list of parameters
+        QueryParams queryParams = new QueryParams();
+        queryParams.getParam().add(queryParam1);
+        queryParams.getParam().add(queryParam2);
+        queryParams.getParam().add(queryParam3);
+
+        // create the Poll object
+        Poll poll = new Poll();
+        poll.setQueryName("SimpleMasterDataQuery");
+        poll.setParams(queryParams);
+        return poll;
     }
 }
